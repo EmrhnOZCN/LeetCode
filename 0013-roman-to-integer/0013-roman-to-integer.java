586ae95add5e8991305684f6d205b3afeb5aa00e
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-22 16:40:26 +0300

    Time: 5 ms (59.47%), Space: 43.6 MB (69.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,33 @@
 class Solution {
-    private static final int[] VALUES = new int[26];
-
-    static {
-        VALUES['I' - 'A'] = 1;
-        VALUES['V' - 'A'] = 5;
-        VALUES['X' - 'A'] = 10;
-        VALUES['L' - 'A'] = 50;
-        VALUES['C' - 'A'] = 100;
-        VALUES['D' - 'A'] = 500;
-        VALUES['M' - 'A'] = 1000;
-    }
-
     public int romanToInt(String s) {
-        char[] chars = s.toCharArray();
-        int total = VALUES[chars[0] - 'A'];
+        // Create a HashMap to store the values of Roman numerals
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        hashMap.put('I', 1);
+        hashMap.put('V', 5);
+        hashMap.put('X', 10);
+        hashMap.put('L', 50);
+        hashMap.put('C', 100);
+        hashMap.put('D', 500);
+        hashMap.put('M', 1000);
 
+        // Convert the input string to a character array for efficient traversal
+        char[] chars = s.toCharArray();
+        // Initialize the total with the value of the first Roman numeral
+        int total = hashMap.get(chars[0]);
+
+        // Iterate through the rest of the characters in the input string
         for (int i = 1; i < chars.length; i++) {
-            if (VALUES[chars[i] - 'A'] > VALUES[chars[i - 1] - 'A']) {
-                total += VALUES[chars[i] - 'A'] - 2 * VALUES[chars[i - 1] - 'A'];
+            // If the current Roman numeral is greater than the previous one
+            if (hashMap.get(chars[i]) > hashMap.get(chars[i - 1])) {
+                // Subtract twice the value of the previous numeral and add the current numeral's value
+                total += hashMap.get(chars[i]) - 2 * hashMap.get(chars[i - 1]);
             } else {
-                total += VALUES[chars[i] - 'A'];
+                // Otherwise, simply add the value of the current numeral to the total
+                total += hashMap.get(chars[i]);
             }
         }
 
+        // Return the final total value
         return total;
     }
 }
