0139e33dd9f221909de5a5909dfbc6a5052bbeba
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-22 16:42:26 +0300

    Time: 5 ms (59.47%), Space: 43.8 MB (53.93%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,33 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        // Create a HashMap to store the values of Roman numerals
-        HashMap<Character, Integer> hashMap = new HashMap<>();
-        hashMap.put('I', 1);
-        hashMap.put('V', 5);
-        hashMap.put('X', 10);
-        hashMap.put('L', 50);
-        hashMap.put('C', 100);
-        hashMap.put('D', 500);
-        hashMap.put('M', 1000);
+    // Create a HashMap to store the values of Roman numerals
+    HashMap<Character, Integer> hashMap = new HashMap<>();
+    hashMap.put('I', 1);
+    hashMap.put('V', 5);
+    hashMap.put('X', 10);
+    hashMap.put('L', 50);
+    hashMap.put('C', 100);
+    hashMap.put('D', 500);
+    hashMap.put('M', 1000);
 
-        // Convert the input string to a character array for efficient traversal
-        char[] chars = s.toCharArray();
-        // Initialize the total with the value of the first Roman numeral
-        int total = hashMap.get(chars[0]);
+    // Initialize the total to keep track of the final integer representation
+    int total = 0;
 
-        // Iterate through the rest of the characters in the input string
-        for (int i = 1; i < chars.length; i++) {
-            // If the current Roman numeral is greater than the previous one
-            if (hashMap.get(chars[i]) > hashMap.get(chars[i - 1])) {
-                // Subtract twice the value of the previous numeral and add the current numeral's value
-                total += hashMap.get(chars[i]) - 2 * hashMap.get(chars[i - 1]);
-            } else {
-                // Otherwise, simply add the value of the current numeral to the total
-                total += hashMap.get(chars[i]);
-            }
+    // Traverse the input string character by character
+    for (int i = 0; i < s.length(); i++) {
+        // Check if we need to subtract the current value
+        if (i < s.length() - 1 && hashMap.get(s.charAt(i)) < hashMap.get(s.charAt(i + 1))) {
+            total -= hashMap.get(s.charAt(i));
+        } else {
+            // Otherwise, add the current value to the total
+            total += hashMap.get(s.charAt(i));
         }
-
-        // Return the final total value
-        return total;
     }
+
+    // Return the final integer representation of the given Roman numeral
+    return total;
+}
+
 }
