b333799c9028db5a085857370165c3e2e4828d5a
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-22 16:26:51 +0300

    Time: 0 ms (100%), Space: 41.4 MB (48.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,42 +11,39 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-         // Dummy düğüm oluşturuluyor, geri dönüş değeri olarak kullanılacak
-        ListNode dummy = new ListNode(-1);
-        // Şu anki düğüm başlangıçta dummy düğümü
-        ListNode current = dummy;
+        // Creating a temp node, which will be used as the return value
+        ListNode temp = new ListNode(-1);
+       
+        // The current node initially points to the temp node
+        ListNode head = temp;
 
-        // Her iki liste de boş olmadığı sürece devam eden bir döngü
+        // Loop until both lists are not empty
         while (list1 != null && list2 != null) {
-            // list1'in değeri list2'den küçük veya eşitse
+            // If the value in list1 is less than or equal to the value in list2
             if (list1.val <= list2.val) {
-                // Şu anki düğümü list1'e bağla
-                current.next = list1;
-                // list1'de bir sonraki düğüme geç
+                // Connect the current node to list1
+                head.next = list1;
+                // Move to the next node in list1
                 list1 = list1.next;
             } else {
-                // list2'nin değeri list1'den küçükse
-                // Şu anki düğümü list2'ye bağla
-                current.next = list2;
-                // list2'de bir sonraki düğüme geç
+                // If the value in list2 is greater than the value in list1
+                // Connect the current node to list2
+                head.next = list2;
+                // Move to the next node in list2
                 list2 = list2.next;
             }
-            // Şu anki düğümü ilerlet
-            current = current.next;
+            // Move the current node forward
+            head = head.next;
         }
 
-        // Bir liste tamamlandığında, diğer listedeki düğümleri sona ekler
+        // Attach the remaining non-null list to the end
         if (list1 != null) {
-            current.next = list1;
+            head.next = list1;
         } else {
-            current.next = list2;
+            head.next = list2;
         }
-
-        // Dummy düğümün bir sonraki düğümü, birleştirilmiş listeyi temsil eder
-        return dummy.next;
-
-
-        
-        
+      
+        // The temp node's next should be the actual start of the merged list
+        return temp.next;
     }
 }
