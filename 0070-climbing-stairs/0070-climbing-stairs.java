ffe5d3e6b5f0b7b245ca8e786c1b7aa8b764d5b0
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-24 23:03:47 +0300

    Time: 0 ms (100%), Space: 38.9 MB (92.38%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int climbStairs(int n ) {
+
+         if(n==1||n==2){
+            return n;
+        }
+
+        int prev = 1;
+
+        int current = 1;
+
+        for(int i = 2 ; i<=n ; i++){
+
+            int temp = current;
+
+            current=prev+current;
+
+            prev=temp;
+
+        }
+
+        return current;
+
+    }
+}
