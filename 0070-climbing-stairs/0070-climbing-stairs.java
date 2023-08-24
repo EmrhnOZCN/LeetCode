4660a7b44cb75237ae3cf9dc1c56a20d4840cf7b
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-24 23:13:19 +0300

    Time: 0 ms (100%), Space: 39.4 MB (39.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,19 @@
 class Solution {
-    public int climbStairs(int n ) {
-
-         if(n==1||n==2){
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        return calculateWays(n, memo);
+    }
+    
+    private int calculateWays(int n, int[] memo) {
+        if (n == 1 || n == 2) {
             return n;
         }
-
-        int prev = 1;
-
-        int current = 1;
-
-        for(int i = 2 ; i<=n ; i++){
-
-            int temp = current;
-
-            current=prev+current;
-
-            prev=temp;
-
+        
+        if (memo[n] != 0) {
+            return memo[n];
         }
-
-        return current;
-
+        
+        memo[n] = calculateWays(n - 1, memo) + calculateWays(n - 2, memo);
+        return memo[n];
     }
 }
