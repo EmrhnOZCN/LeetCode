b0fee2a7fe0ad6e6e0053556ebfc9de879383b5c
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-25 21:05:21 +0300

    Time: 3 ms (67.13%), Space: 40.6 MB (77.17%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -8,21 +8,22 @@ class Solution {
             return false;
         }
         
-        boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i > 0 && s1.charAt(i - 1) == s3.charAt(i + j - 1)) {
-                    dp[i][j] = dp[i][j] || dp[i - 1][j];
-                }
-                if (j > 0 && s2.charAt(j - 1) == s3.charAt(i + j - 1)) {
-                    dp[i][j] = dp[i][j] || dp[i][j - 1];
-                }
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        
+        for (int j = 1; j <= n; j++) {
+            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+                        || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
             }
         }
-
-        return dp[m][n];
+        
+        return dp[n];
 
         
     }
