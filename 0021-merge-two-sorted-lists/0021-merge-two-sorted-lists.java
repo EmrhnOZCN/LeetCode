11ba865a8d64160d39fcac52a89fcde2083ea367
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-26 20:37:44 +0300

    Time: 0 ms (100%), Space: 41.4 MB (58.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,39 +11,38 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        // Creating a temp node, which will be used as the return value
-        ListNode temp = new ListNode(-1);
+        
+        ListNode temp = new ListNode(-5);
        
-        // The current node initially points to the temp node
+     
         ListNode head = temp;
 
-        // Loop until both lists are not empty
+      
         while (list1 != null && list2 != null) {
-            // If the value in list1 is less than or equal to the value in list2
+         
             if (list1.val <= list2.val) {
-                // Connect the current node to list1
-                head.next = list1;
-                // Move to the next node in list1
+             head.next = list1;
+             
                 list1 = list1.next;
             } else {
-                // If the value in list2 is greater than the value in list1
-                // Connect the current node to list2
+               
                 head.next = list2;
-                // Move to the next node in list2
+              
                 list2 = list2.next;
             }
-            // Move the current node forward
+          
             head = head.next;
         }
 
-        // Attach the remaining non-null list to the end
+      
         if (list1 != null) {
             head.next = list1;
-        } else {
+        }
+         else {
             head.next = list2;
         }
       
-        // The temp node's next should be the actual start of the merged list
+        
         return temp.next;
     }
 }
