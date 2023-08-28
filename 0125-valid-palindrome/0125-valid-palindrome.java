43c7116d77c1efc5d76ef404a6cc12e8b1b875b5
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-08-28 11:19:10 +0300

    Time: 4 ms (64.08%), Space: 43.6 MB (56.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,47 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+   public boolean isPalindrome(String s) {
 
-
-        String newStr = s.replaceAll("[^a-zA-Z0-9]", "").replace(" ","").toLowerCase();
-
-
-
-
-
-
-        StringBuilder stringBuilder = new StringBuilder(newStr);
-
-        String reversStr = stringBuilder.reverse().toString();
-
-
-
-        if(reversStr.equals(newStr)){
+        if(s.isEmpty()){
 
             return true;
         }
 
-        return false;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Character c:s.toCharArray()
+             ) {
+
+            if(Character.isLetterOrDigit(c)){
+                stringBuilder.append(Character.toLowerCase(c));
+            }
+
+
+
+        }
+
+
+        int left = 0;
+        int right = stringBuilder.length() - 1;
+
+
+        while (left<right){
+
+
+            if(stringBuilder.charAt(left) != stringBuilder.charAt(right)) {
+
+
+
+                return false;
+            }
+
+            left++;
+
+            right--;
+
+        }
+
+
+
+        return true;
 
     }
 }
