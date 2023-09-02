450957cf502a20d7f846bb058e104ba0a186a321
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-09-02 22:17:50 +0300

    Time: 2 ms (99.57%), Space: 41.9 MB (78.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,47 +1,29 @@
 class Solution {
    public boolean isPalindrome(String s) {
-
-        if(s.isEmpty()){
-
+        if (s.isEmpty()) {
             return true;
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Character c:s.toCharArray()
-             ) {
-
-            if(Character.isLetterOrDigit(c)){
-                stringBuilder.append(Character.toLowerCase(c));
-            }
-
-
-
-        }
-
-
         int left = 0;
-        int right = stringBuilder.length() - 1;
+        int right = s.length() - 1;
 
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
 
-        while (left<right){
-
-
-            if(stringBuilder.charAt(left) != stringBuilder.charAt(right)) {
-
-
-
-                return false;
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
             }
-
-            left++;
-
-            right--;
-
         }
-
-
 
         return true;
-
     }
 }
