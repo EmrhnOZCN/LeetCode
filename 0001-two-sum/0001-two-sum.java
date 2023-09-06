4754f811b28a1d0462fd2a4c4d62fc2c2be01432
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-09-06 08:40:15 +0300

    Time: 1 ms (99.42%), Space: 43.7 MB (62.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,38 +3,20 @@ class Solution {
 
         Map<Integer,Integer> hashMap =  new HashMap<>();
 
-
         for(int i = 0 ; i<nums.length;i++){
-
-
-
 
             int result = target-nums[i];
 
-
-
-
             if(hashMap.containsKey(result)){
 
-
                 return new int[]{hashMap.get(result),i};
-
-
-
-
 
 
             }
 
             hashMap.put(nums[i],i);
 
-
         }
-
-
-
-
-        
 
         return new int[] {};
         
