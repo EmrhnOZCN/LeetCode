483ddeda4d23085d0e2122d51351079093b8433c
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-09-11 11:27:38 +0300

    Time: 2 ms (87.31%), Space: 40.7 MB (88.96%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,25 +1,43 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordSet = new HashSet<>(wordDict);
-
-        int n = s.length();
-
-        boolean[] dp = new boolean[n+1];
-
-        dp[0] = true;
-
-        for(int i = 0; i<=n;i++){
-
-            for(int j = 0 ; j<i ; j++){
-
-                if(dp[j] == true && wordSet.contains(s.substring(j,i))){
-                    dp[i] = true;
-
-                    break;
+	// for memoization
+        HashMap<String,Boolean> map= new HashMap<>();
+        return canConstruct(s, wordDict, map);
+    }
+    
+    public boolean canConstruct(String target,List<String> words, HashMap<String,Boolean> map)
+    {
+	
+	// if answer already cached, return it
+        if(map.containsKey(target))
+            return map.get(target);
+			
+		// if target string is empty
+		// it can always be constructed by taking no elements from dictionary
+        if(target.isEmpty())
+            return true;
+			
+		// for all words in the dictionary
+        for(String word: words)
+        {
+		// if the target starts with the given word
+            if(target.startsWith(word))
+            {
+			// and it is possible to construct the rest of the string
+			// from the words in the dictionary
+                if(canConstruct(target.substring(word.length()),words, map))
+                {
+				
+				// save and return true
+                    map.put(target, true);
+                    return true;
                 }
             }
         }
-
-        return dp[n];
+		
+		// if it was not possible to construct the target from words from the dictionary
+		// save and return false to the previous call
+        map.put(target,false);
+        return false;
     }
 }
