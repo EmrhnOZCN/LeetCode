56cb48bc101c912c1610d297de287b2581894d8f
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-09-13 21:16:09 +0300

    Time: 3 ms (11.3%), Space: 57 MB (96.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,24 +4,15 @@ class Solution {
             return 0;
         }
 
-        int maxProfit = 0; // Maksimum karı saklayacak değişken
-        int minPrice = prices[0]; // Şu ana kadar görülen en düşük fiyatı saklayacak değişken
+        int n = prices.length;
+        int[] dp = new int[n]; // dp[i], i. günün sonunda elde edilebilecek maksimum karı saklar
+        int minPrice = prices[0]; // Şu ana kadar görülen en düşük fiyatı saklar
 
-        for (int i = 1; i < prices.length; i++) {
-            int currentPrice = prices[i];
-            // Şu ana kadar görülen en düşük fiyatı güncelle
-            if (currentPrice < minPrice) {
-                minPrice = currentPrice;
-            } else {
-                // Şu ana kadar görülen en düşük fiyattan alıp bu fiyattan sattığımızda karı hesapla
-                int profit = currentPrice - minPrice;
-                // Eğer bu kar, daha önceki maksimum karı geçiyorsa güncelle
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
-            }
+        for (int i = 1; i < n; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
         }
 
-        return maxProfit;
+        return dp[n - 1];
     }
 }
