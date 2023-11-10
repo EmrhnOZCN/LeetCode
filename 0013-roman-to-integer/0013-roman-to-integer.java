19db8e59edc5efff11acf2249232325b0c54a280
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-11-10 11:36:13 +0300

    Time: 4 ms (65.97%), Space: 43.6 MB (62.73%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,31 +1,36 @@
 class Solution {
     public int romanToInt(String s) {
-    // Create a HashMap to store the values of Roman numerals
-    HashMap<Character, Integer> hashMap = new HashMap<>();
-    hashMap.put('I', 1);
-    hashMap.put('V', 5);
-    hashMap.put('X', 10);
-    hashMap.put('L', 50);
-    hashMap.put('C', 100);
-    hashMap.put('D', 500);
-    hashMap.put('M', 1000);
+  HashMap<Character,Integer> hashMap = new HashMap<>();
 
-    // Initialize the total to keep track of the final integer representation
-    int total = 0;
 
-    // Traverse the input string character by character
-    for (int i = 0; i < s.length(); i++) {
-        // Check if we need to subtract the current value
-        if (i < s.length() - 1 && hashMap.get(s.charAt(i)) < hashMap.get(s.charAt(i + 1))) {
-            total -= hashMap.get(s.charAt(i));
-        } else {
-            // Otherwise, add the current value to the total
-            total += hashMap.get(s.charAt(i));
+        hashMap.put('I',1);
+        hashMap.put('V',5);
+        hashMap.put('X',10);
+        hashMap.put('L',50);
+        hashMap.put('C',100);
+        hashMap.put('D',500);
+        hashMap.put('M',1000);
+
+        int total =0;
+
+        for (int i = 0 ; i<s.length();i++){
+
+            if ((i<s.length()-1 ) && hashMap.get(s.charAt(i))<hashMap.get(s.charAt(i+1)) ){
+
+                total-=hashMap.get(s.charAt(i));
+
+
+            }
+            else {
+
+                total+=hashMap.get(s.charAt(i));
+            }
         }
+
+
+
+
+      return total;
+
     }
-
-    // Return the final integer representation of the given Roman numeral
-    return total;
-}
-
 }
