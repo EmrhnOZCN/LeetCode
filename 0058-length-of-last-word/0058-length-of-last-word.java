12e18c90ec00cd9b9434e64a10cf70bbcb83ea7f
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-11-11 14:19:31 +0300

    Time: 0 ms (100%), Space: 40.5 MB (64.28%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -5,34 +5,18 @@ class Solution {
         for(int i = s.length()-1 ; i>0;i--){
 
 
-
-
             if(s.charAt(i)!=' ' && s.charAt(i-1) == ' ' ){
-
-              
 
                  first = i;
 
-              
-
-
                 break;
-
 
 
             }
 
 
-
         }
-
-
-        String temp  = s.substring(first).trim();
-
-
-     
-
-        return temp.length();
+        return  s.substring(first).trim().length();
     }
         
     }
