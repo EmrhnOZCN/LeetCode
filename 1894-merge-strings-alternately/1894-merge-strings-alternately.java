656a8aab12279eb5037c555d2778fe3b0f6dcdab
Emirhan Özcan <98975826+EmrhnOZCN@users.noreply.github.com>
2023-11-12 20:48:56 +0300

    Time: 1 ms (78.26%), Space: 40.1 MB (97.45%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,6 +1,5 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-
         StringBuilder result = new StringBuilder();
         int maxLength = Math.max(word1.length(), word2.length());
 
@@ -14,6 +13,7 @@ class Solution {
         }
 
         return result.toString();
-        
+
+
     }
 }
